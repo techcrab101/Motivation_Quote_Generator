a16a84963046b1fee358b00cd71347525ce7f6e4
techcrab101 <techcrab101@gmail.com>
2017-12-14 10:59:12 -0500

    Added some font control

--- main.py
+++ main.py
@@ -8,11 +8,14 @@ text_path = 'json.txt'
 img_path = 'images/'
 
 phrase = generate_text.make_phrase(text_path)
-print(phrase)
+
+print('\n' + phrase + '\n')
+
 list_phrase = phrase.split()
 
 img_path += random.choice(os.listdir(img_path))
-print(img_path)
+
+print('Image path:' , img_path + '\n')
 
 img = cv2.imread(img_path)
 
@@ -21,12 +24,30 @@ img = generate_images.carve_image(img, size = 320)
 img = cv2.GaussianBlur(img, (5,5), 0)
 
 char_len = len(phrase)
-print(char_len)
-print (list_phrase)
-print(len(list_phrase))
 
-img =cv2.putText(img=img, text=phrase, org=(200,200),fontFace=3, fontScale=3, color=(0,0,0), thickness=3)
-img =cv2.putText(img=img, text=phrase, org=(200,200),fontFace=3, fontScale=3, color=(255,255,255), thickness=1)
+print('\n' + 'char length: ' + str(char_len))
+print(list_phrase)
+
+new_list_phrase = ''
+for i, phrase in enumerate(list_phrase):
+    s = ''
+    if i % 3 == 0 and i != 0:
+        s = '\n'
+    new_list_phrase += phrase +' ' + s
+
+print('phrase length:', len(list_phrase))
+
+font_size = ( 1 / char_len ) * 100
+
+font_size = 3 if font_size > 3 else font_size
+
+print('font size:', font_size)
+
+print()
+print(new_list_phrase)
+
+img =cv2.putText(img=img, text=new_list_phrase, org=(200,200),fontFace=3, fontScale=font_size, color=(0,0,0), thickness=3)
+img =cv2.putText(img=img, text=new_list_phrase, org=(200,200),fontFace=3, fontScale=font_size, color=(255,255,255), thickness=1)
 
 cv2.imshow('result', img)
 cv2.waitKey(0)
